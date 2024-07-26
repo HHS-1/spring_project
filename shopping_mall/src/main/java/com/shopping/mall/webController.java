4b9aa5c647560b729509b979ada5395f78121fe8
HHS-1 <qazplm1021@naver.com>
2024-07-26 11:31:37 +0900

    first commit

---
+++ shopping_mall/src/main/java/com/shopping/mall/webController.java
@@ -0,0 +1,13 @@
+package com.shopping.mall;
+
+import java.io.PrintWriter;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class webController {
+
+	PrintWriter pw = null;
+	
+	
+}
