970c3b3347d871c2ac189e9a71c71dfff0653bdb
HHS <77200506+HHS-1@users.noreply.github.com>
2024-07-29 14:06:18 +0900

    아이디 찾기, 패스워드 찾기

---
+++ shopping_mall/src/main/java/com/shopping/mall/user_dao.java
@@ -0,0 +1,12 @@
+package com.shopping.mall;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class user_dao {
+	int uidx;
+	String uid, upass, uname, in_date;
+}
--- shopping_mall/src/main/java/com/shopping/mall/webController.java
+++ shopping_mall/src/main/java/com/shopping/mall/webController.java
@@ -1,9 +1,14 @@
 package com.shopping.mall;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.json.JSONArray;
@@ -18,9 +23,58 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
-public class webController {
+public class webController extends md5_pass {
 
 	PrintWriter pw = null;
+	@Resource(name = "userselect")
+	private user_select us;
+	@PostMapping("/passmodify.do")
+	public String passsmodify() {
+			
+		return null;
+	}
+	
+	
+	@PostMapping("/idsearch.do")
+	public String idsearch(@RequestParam String uname, String uemail, HttpServletResponse res) throws Exception {
+		res.setContentType("text/html; charset=utf-8");
+
+		System.out.println(uname);
+		System.out.println(uemail);
+		this.pw = res.getWriter();
+		
+		try {
+			if(uname == null || uemail == null) {
+				this.pw.print("<script>"
+						+ "alert('올바른 접근방식이 아닙니다.');"
+						+ "history.go(-1);"
+						+ "</script>");
+			}else {
+				ArrayList<Object> onedata = us.search_id(uname, uemail);
+				System.out.println(onedata);
+			}
+		}catch(Exception e) {
+			System.out.println(e);
+			this.pw.print("<script>"
+					+ "alert('에러');"
+					+ "history.go(-1);"
+					+ "</script>");
+		}finally {
+			
+		}
+		
+		return null;
+	}
+
+	//패스워드 변경 여부를 체크(MD5)
+	@GetMapping("/passwd.do")
+	public String passwd() {
+		String pwd = "a1234";
+		String result = this.md5_making(pwd);
+		System.out.println(result);
+		return null;
+	}
+	
 	
 	//@SessionAttribute : session이 이미 등록되어 있는 상황일 경우 해당 정보를 가져올 수 있음
 	@GetMapping("/restapi.do")
